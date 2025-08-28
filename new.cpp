5fef3af8404aa6e798f6a16257f5d30f95f393af
Since2024 <hasonmagar@gmail.com>
2025-08-28 13:56:24 +0545

    proposal

---
+++ new.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <cstdint>
+#include <cassert>
+
+uint32_t pack_color(const uint_t r, const uint8_t g, const uint8_t b, const uint8_t a=255) { 
+    return (a<<2on
+}
