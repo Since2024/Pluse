f97757ace350833062abe4d336eff03a4f0bc99e
Since2024 <hasonmagar@gmail.com>
2026-01-01 15:40:16 +0545

    redone

--- SnakeGame/snakeKing.cpp
+++ SnakeGame/snakeKing.cpp
@@ -1,47 +1,22 @@
-#include<iostream>
-#include<conio.h>
-#include<Windows.h>
+#include <iostream>
+#include <raylib.h>
+
 using namespace std;
 
-enum Direction{STOP = 0, LEFT, RIGHT, UP, DOWN};
-Direction dir;
-bool gameOver;
-const int height = 20;
-const int width = 20;
-int headX, headY, fruitX, fruitY, score;
-int tailx[100], taily[100];
-int tail_len;
+int main () {
 
-void setup();
-void draw();
-void input();
-void logic();
+    cout<<"Starting the game..."<<endl;
+    InitWindow(750, 750, "Retro Snake");
+    SetTargetFPS(60);
 
-int main(){
-    char start;
-    cout << "\t-------------------------------" << endl;
-    cout << "\t\t :Snake King:" << endl;
-    cout << "\t-------------------------------" << endl;
-    cout << "\tPress 's' to option: ";
-    cin >> start;
-    if(start == 's'){
-        setup();
-        while(!gameOver){
-            draw();
-            input();
-            logic();
-            Sleep(30);
-            system("cls");
-        }
+    while(WindowShouldClose() == fasle)
+    {
+        BeginDrawing();
+
+        EndDrawing();
     }
-}
 
-void setup(){
-    gameOver = false;
-    dir = STOP;
-    headX = width/2;
-    headY = height/2;
-    fruitX = rand()%width;
-    fruitY = rand()%height;
-    score = 0;
+    CloseWindow();
+    return 0;
+   
 }
