36d14f65392586fdf3c7fbd5408084f3acc344b4
Since2024 <hasonmagar@gmail.com>
2026-01-05 23:52:20 +0545

    prac

--- new.cpp
+++ new.cpp
@@ -1,11 +1,34 @@
 //simple interest
+// #include <iostream>
+// using namespace std;
+
+// int main(){
+//     float p = 1000;
+//     float t = 3;
+//     float r = 30;
+//     float si = (p*t*r/100);
+//     cout <<si;
+// }
+
+// #include <iostream>
+// using namespace std;
+
+// int main() {
+//     char ch;
+//     cin>>ch;
+//     cout<<(int)ch;
+// }
+    
+//take positive integer input and tell if it is even or odd.
 #include <iostream>
 using namespace std;
 
-int main(){
-    float p = 1000;
-    float t = 3;
-    float r = 30;
-    float si = (p*t*r/100);
-    cout <<si;
+int main() {
+    int n;
+    cin>>n;
+    if(n%2==0)
+        cout<<"Even number";
+    else
+        cout<<"Odd number";
+        
 }
