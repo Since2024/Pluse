57fc99bdd7610fad0d568825bdb8f85f3a4f93b3
Since2024 <hasonmagar@gmail.com>
2026-01-05 21:01:43 +0545

    simple interest

---
+++ new.cpp
@@ -0,0 +1,11 @@
+//simple interest
+#include <iostream>
+using namespace std;
+
+int main(){
+    float p = 1000;
+    float t = 3;
+    float r = 30;
+    float si = (p*t*r/100);
+    cout <<si;
+}
